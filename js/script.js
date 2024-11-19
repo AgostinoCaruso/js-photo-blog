d745410f9f6d46ed8fa7b60352df0bb1e1e12858
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-19 16:51:57 +0100

    js axios done

--- js/script.js
+++ js/script.js
@@ -13,4 +13,54 @@ rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano
 */
 
 
+const baseUrl = "https://jsonplaceholder.typicode.com/";
+const resource = "photos";
+const numImg = 6;
+const params = { "_limit": numImg };
 
+const eleDivContainer = document.querySelector(".container");
+FuncAxios();
+function FuncAxios() {
+    axios.get(baseUrl + resource, { params })
+        .then((response) => {
+            const photos = response.data;
+
+            photos.forEach(photo => {
+                const urlImg = photo.url;
+                const urlText = photo.title;
+                CreateCard(urlImg, urlText);
+            });
+        })
+        .catch((error) => {
+            console.error("Errore nella chiamata API:", error);
+        });
+}
+const template = "";
+
+function CreateCard(img,text) {
+
+    const cardTemplate = `
+    <div class="card debugger">
+        <span class="pinAbsolute"><img src="./img/pin.svg" alt=""></span>
+        <div class="card-img debugger">
+            <img src="${img}" alt="test">
+        </div>
+        <div class="card-text debugger">
+            ${text}
+        </div>
+    </div>
+    `;
+    eleDivContainer.innerHTML +=cardTemplate;
+}
+/*
+    <div class="card debugger">
+        <span class="pinAbsolute"><img src="./img/pin.svg" alt=""></span>
+        <div class="card-img debugger">
+            <img src="https://picsum.photos/200/200" alt="test">
+        </div>
+        <div class="card-text debugger">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas fugit tempora quia veniam quaerat
+            doloribus eum inventore rerum voluptatibus reprehenderit.
+        </div>
+    </div>
+*/
